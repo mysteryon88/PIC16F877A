7ab85254295091a7805b75e4d3861080301bbdb7
mysteryon88 <mysteryon88@yandex.ru>
2021-11-04 05:14:11 +0300

    update task25

--- task25/task25.c
+++ task25/task25.c
@@ -1,15 +1,8 @@
 #include "task25.h"
 
-#define TRUE 1
-
-#define ASCENDING "   ASCENDING    " 
-#define DESCENDING "   DESCENDING   "
-#define NOT_ORDERED "  NOT ORDERED   "
-#define N 7
-
 void start(void){
 	byte decision = 0x00;
-	/*
+	Clr_LCD();
 	if (eeprom_decision()) {//вывод из eeprom или само задание
 		char str_to_lcd[32] = " ";
 		loadstr(str_to_lcd);
@@ -19,17 +12,15 @@ void start(void){
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 7))) decision = 1;
 		} while (!decision);
-		task();
 	}
-	else task();
-	*/
+	Clr_LCD();
 	task();
 }
 
 //звонок
 void Beep(void){ 
 	byte tmp_TRISB=TRISB, tmp_PORTB=PORTB, i;
-	TRISB3=0; // clrbit (TRISB,3);
+	TRISB3=0; 
 	i=100000; 
 	while(i--){
  		RB3=1;
@@ -49,18 +40,19 @@ void Overflow(void){
 void task(void){
 	short num[N];
 	unsigned short i = 0x00, j = 0x00;
-	char up_str[N];
+	char up_str[N], str_to_eeprom[32] = " ";
 	byte decision = 0x00;
 	short duration = 0x00;
 	short c_asc = 0x00, c_desc = 0x00, c_not = 0x00;
 	while (TRUE){
-		decision = 0x00;
 		for(i = 0; i < N; i++){
 			num[i] = -1;
 			up_str[i] = ' ';	
 		}
 		Clr_LCD();
 		i = 0x00;
+		Set_Coord_LCD(0, 4);
+		Show_String_LCD("0");
 		do {
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 4))){
@@ -82,18 +74,26 @@ void task(void){
 			if (!(PORTB & (1 << 7))) decision = 1;	//exit	
 		} while (!decision);
 		
+		c_asc = 0x00, c_desc = 0x00, c_not = 0x00;
+	
 		for (j = 0; j < i; j++)
 			(num[j]<num[j+1]) ? c_asc++ : (num[j]>num[j+1]) ? c_desc++ : c_not++;
 		
-		if(c_asc + c_not == i) {
+		strcpy(str_to_eeprom, "    "); //4
+		strcat(str_to_eeprom, up_str); //7
+		strcat(str_to_eeprom, "     ");//5
+		
+		if(c_asc == i) {
 			duration = 3;
 			while(duration--) Beep();
 			Set_Coord_LCD(1, 0);
 			Show_String_LCD(ASCENDING);
+			strcat(str_to_eeprom, ASCENDING);
 		}
-		else if (c_desc + c_not == i){
+		else if (c_desc == i){
 			Set_Coord_LCD(1, 0);
 			Show_String_LCD(DESCENDING);
+			strcat(str_to_eeprom, DESCENDING);
 			for (j = 0; j < 2; j++){
 				duration = 4;
 				while(duration--) Beep();
@@ -103,14 +103,20 @@ void task(void){
 		else {
 			Set_Coord_LCD(1, 0);
 			Show_String_LCD(NOT_ORDERED);
+			strcat(str_to_eeprom, NOT_ORDERED);
 			duration = 15;
 			while(duration--) Beep();
 		}
 		do {
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 7))) break;
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 6))){
+				Clr_LCD();
+				savestr(str_to_eeprom);	
+				Show_String_LCD(SAVE);
+			}
 		} while (TRUE);
+		decision = 0x00;
 	}
 }
-
-
--- task25/task25.h
+++ task25/task25.h
@@ -3,6 +3,14 @@
 
 #include "LCD.h"
 
+#define TRUE 1
+
+#define ASCENDING "   ASCENDING    " 
+#define DESCENDING "   DESCENDING   "
+#define NOT_ORDERED "  NOT ORDERED   "
+#define SAVE "     SAVED"
+#define N 7
+
 void start(void);//начало
 void task(void);//задание
 void Beep(void);
