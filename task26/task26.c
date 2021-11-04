f2b1548023f91a3e0b7b1949489c85c31236b06f
mysteryon88 <mysteryon88@yandex.ru>
2021-11-04 05:29:38 +0300

    update task26

--- task26/task26.c
+++ task26/task26.c
@@ -1,10 +1,8 @@
 #include "task26.h"
 
-#define TRUE 1
-
 void start(void){
 	byte decision = 0x00;
-	/*
+	Clr_LCD();
 	if (eeprom_decision()) {//вывод из eeprom или само задание
 		char str_to_lcd[32] = " ";
 		loadstr(str_to_lcd);
@@ -14,10 +12,9 @@ void start(void){
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 7))) decision = 1;
 		} while (!decision);
-		task();
+	    Clr_LCD();
 	}
-	else task();
-	*/
+	Clr_LCD();
 	task();
 }
 
@@ -45,8 +42,12 @@ void Show_LCD(short num, short j){
 
 void task(void){
 	short num = 0x00, sum = 0x00;
+	byte str_to_eeprom[32] = " ", num_[5] = " ",  sum_[4] = " ";
 	byte decision = 0x00;
 	while (TRUE){
+		memset(num_, '\0', 5);
+		memset(sum_, '\0', 4);
+		Show_LCD(num, 0);
 		decision = 0x00;
 		do {
 			PORTB |= 0xF0;
@@ -67,25 +68,50 @@ void task(void){
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 7))) decision = 1;		
 		} while (!decision);
-
+		
+		sprintf(num_, "%d", num); 
 		//calculate the amount
 		while (num!=0){
       		sum += num % 10;
 			num /= 10;
     	}
-
-		Show_LCD(sum, 1);
-		int p = 10;
 		
-		if(!(sum % 10)) while(p--) Beep();
-		do {
+		sprintf(sum_, "%d", sum); 
+		Show_LCD(sum, 1);
 
+		int p = 10;		
+		if(!(sum % 10)) while(p--) Beep();
+		
+		do {
 			PORTB |= 0xF0;
 			if (!(PORTB & (1 << 7))) break;
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 6))){
+				make_string(str_to_eeprom, num_, sum_);
+				Clr_LCD();
+				savestr(str_to_eeprom);	
+				Show_String_LCD(SAVE);
+			}
 		} while (TRUE);
-		num = 0;
-		sum = 0;
+		memset(str_to_eeprom, '\0', 32);
+		num = 0x00;
+		sum = 0x00;
 		Clr_LCD();
 	}
+}
+
+void make_string(char* str_to_eeprom, char* num_, char* sum_){
+	short i = 0x00, j = 0x00;
+	strcpy(str_to_eeprom, "      ");//6
+	strcat(str_to_eeprom, num_);
+	while(num_[i]) i++;	
+	for (j = 10; j > i; j--)
+		strcat(str_to_eeprom, " ");
+	strcat(str_to_eeprom, "      ");//6 
+	strcat(str_to_eeprom, sum_);
+	i = 0x00;
+	while(num_[i]) i++;	
+	for (j = 10; j > i; j--)
+		strcat(str_to_eeprom, " ");
 }
 
--- task26/task26.h
+++ task26/task26.h
@@ -3,8 +3,12 @@
 
 #include "LCD.h"
 
+#define TRUE 1
+#define SAVE "     SAVED"
+
 void start(void);//начало
 void task(void);//задание
 void Beep(void);
 void Show_LCD(short num, short j);
+void make_string(char* str_to_eeprom, char* num_, char* sum_);
 #endif
