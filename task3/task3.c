0233d05a7fc3c44500ba4359f2e02487334700fd
mysteryon88 <mysteryon88@yandex.ru>
2021-12-17 13:47:08 +0300

    add task3

---
+++ task3/task3.c
@@ -0,0 +1,137 @@
+#include "task3.h"
+
+void interrupt timer0(void){
+	if (time == 40){
+		Clr_LCD();
+		if(all){
+			Set_Coord_LCD(ind/16, ind%16);
+			Show_String_LCD(ticker);
+		}
+		else {
+			if(up_down)	Set_Coord_LCD(1, ind%16);
+			else Set_Coord_LCD(ind/16, ind%16);
+			Show_String_LCD_(ticker);
+		}
+		if(direction){	
+			if(!all) ind = (ind + 1) % 16;
+			if(all) ind = (ind + 1) % 32;
+		}
+		else {
+			if(!all) (ind == 0) ? ind = 15 : ind--;
+			if(all) (ind == 0) ? ind = 31 : ind--;
+		}
+		time = 0;
+	}	
+	time++;
+    T0IF=0x00;
+}
+
+void start(void){
+	byte decision = 0x00;
+	
+	if (eeprom_decision()) {//вывод из eeprom или само задание
+		char str[6] = " ";
+		loadstr_(str);
+		if(str[1] == '-') ind = str[0] - 48;
+		else ind = 10 * (str[0] - 48) + str[1] - 48;
+		up_down = str[2] - 48;
+		all = str[3] - 48;
+		direction = str[4] - 48;
+		/*
+		Clr_LCD();
+		Show_String_LCD(str);
+		do {
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 7))) decision = 1;
+		} while (!decision);
+		*/
+	}
+	Clr_LCD();
+	OPTION_REG=0x07; //TMR0 and prescaler
+	INTCON=0xA0; 	 //прерывания по переполнению TMR0
+	task();
+}
+
+void task(void){
+	short num = 0x00;
+	byte decision = 0x00;
+	int jj = 0x00;
+	
+	while (TRUE){
+		decision = 0x00;
+
+		PORTB |= 0xF0;
+		if (!(PORTB & (1 << 4))){
+				PORTB |= 0xF0;
+				if (!(PORTB & (1 << 4))) direction = direction ? 0x00 : 0x01;
+		}	
+
+		PORTB |= 0xF0;
+		if (!(PORTB & (1 << 5))){
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 5))){
+				if(up_down){
+					up_down	= 0x00;
+					ind = 0;
+				}
+				else {
+					up_down = 0x01;
+					ind = 16;
+				}
+				all = 0x00;
+			}	
+		}
+		PORTB |= 0xF0;
+		if (!(PORTB & (1 << 6))){
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 6))) all = 0x01;
+		}		
+		PORTB |= 0xF0;
+		if (!(PORTB & (1 << 7))){
+			PORTB |= 0xF0;
+			if (!(PORTB & (1 << 7))) save_ind();
+		}	
+	}
+}
+
+
+void Send_Byte_LCD_(byte tmp) {
+    while (Check_buttons())
+    	Delay(1000);
+    PORTB = (PORTB & 0x0F)+ (tmp & 0xF0);
+    Pulse(10);
+    PORTB = (PORTB & 0x0F)+ (tmp << 4);
+    Pulse(10);
+    Current_ind++;
+    if (Current_ind == 16)Set_Coord_LCD(up_down, 0);
+    if (Current_ind == 32)Set_Coord_LCD(up_down, 0);
+}
+
+void Show_String_LCD_(const char * mySTRING) {
+    while (*mySTRING) {
+        Send_Byte_LCD_(*(mySTRING++));
+    };
+}
+
+void save_ind(void){
+	char ind_str[6] = " ";
+	sprintf(ind_str, "%d", ind); //сохраняем индекс 1-2 символа
+	if (ind < 10) ind_str[1] = '-'; 
+	ind_str[2] = 48 + up_down;   //какая строка
+	ind_str[3] = 48 + all;       //обе ли строки
+	ind_str[4] = 48 + direction; //направление
+	ind_str[5] = '\0'; //направление
+	savestr(ind_str);
+}
+
+void loadstr_(char * mySTRING) {
+    Init_READ_I2C(0);
+	
+	int i = 0;
+   	for (i = 0; i < 5; i++)
+    	mySTRING[i] = IN_BYTE_ACK_I2C();
+
+    IN_BYTE_NACK_STOP_I2C();
+}
+
+
---
+++ task3/task3.h
@@ -0,0 +1,28 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+#include "LCD.h"
+
+#define TRUE 1
+
+void start(void);//начало
+void task(void);//задание
+void Beep(void);
+void Show_String_LCD_(const char * mySTRING);
+void Send_Byte_LCD_(byte tmp);
+void interrupt timer0(void);
+void save_ind(void);
+void loadstr_(char * mySTRING);
+
+char ticker[] = "Sell garage";
+//down - 1 up - 0
+byte up_down = 0x01;
+//1 - two string
+//0 - one string
+byte all = 0x00;
+//1 - right 0 - left
+byte direction = 0x01;
+short ind = 0x00;
+unsigned short time = 0x00;
+
+#endif
